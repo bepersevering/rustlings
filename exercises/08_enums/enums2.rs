9e99837593fb7d1d6941c7e46662b4a96456c628
bepersevering <mingshashan@gmail.com>
2025-01-29 22:03:51 +0800

    exercises

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -29,10 +29,7 @@ impl Message {
 
 fn main() {
     let messages = [
-        Message::Resize {
-            width: 10,
-            height: 30,
-        },
+        Message::Resize(Resize {width: 10, height: 10}),
         Message::Move(Point { x: 10, y: 15 }),
         Message::Echo(String::from("hello world")),
         Message::ChangeColor(200, 255, 255),
