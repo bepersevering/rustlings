ffaf5f33b736b3e4510cf5117b0f6b6db64c11cf
bepersevering <mingshashan@gmail.com>
2025-02-12 16:13:45 +0800

    exercises

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -34,7 +34,29 @@ impl Default for Person {
 // 5. Parse the second element from the split operation into a `u8` as the age.
 // 6. If parsing the age fails, return the default of `Person`.
 impl From<&str> for Person {
-    fn from(s: &str) -> Self {}
+    fn from(s: &str) -> Self {
+        let parts: Vec<&str> = s.split(",").collect();
+         // 判断分割后的元素数量是否为2
+         if parts.len() != 2 {
+            return Person::default();
+        }
+
+        let name = parts[0];
+        if (name.is_empty()) {
+            return Person::default();
+        }
+        
+        let age_result = parts[1].parse::<u8>();
+
+        if let Err(_) = age_result {
+            return Person::default();
+        }
+
+        Person {
+            name : name.to_string(),
+            age: age_result.unwrap()
+        }
+    }
 }
 
 fn main() {
